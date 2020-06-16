45750bef0b50c94534656ee055bfa042169d3d0c
Yafei Chen <yafchen@thoughtworks.com>
2020-06-16 18:46:52 +0800

    [yafei] primary locker robot: save to first available locker

--- src/test/java/com/tw/PrimaryLockerRobotTest.java
+++ src/test/java/com/tw/PrimaryLockerRobotTest.java
@@ -18,4 +18,16 @@ public class PrimaryLockerRobotTest {
         assertNotNull(ticket);
         assertSame(myBag, firstLocker.pickUpBy(ticket));
     }
+
+    @Test
+    public void should_return_ticket_and_save_in_2nd_locker_when_save_bag_given_robot_manage_two_lockers_and_1st_locker_is_full_and_2nd_locker_has_capacity() {
+        Locker secondLocker = new Locker(15);
+        PrimaryLockerRobot robot = new PrimaryLockerRobot(asList(new Locker(0), secondLocker));
+
+        Bag myBag = new Bag();
+        Ticket ticket = robot.save(myBag);
+
+        assertNotNull(ticket);
+        assertSame(myBag, secondLocker.pickUpBy(ticket));
+    }
 }
