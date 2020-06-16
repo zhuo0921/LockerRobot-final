28b362485504b3eda4b417a27da90a17a1c6468e
Yafei Chen <yafchen@thoughtworks.com>
2020-06-16 18:42:39 +0800

    [yafei]primary locker robot: save to 1st locker when both lockers have capacity

---
+++ src/test/java/com/tw/PrimaryLockerRobotTest.java
@@ -0,0 +1,21 @@
+package com.tw;
+
+import org.junit.Test;
+
+import static java.util.Arrays.asList;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertSame;
+
+public class PrimaryLockerRobotTest {
+    @Test
+    public void should_return_ticket_and_save_in_1st_locker_when_save_bag_given_robot_manage_two_lockers_and_both_have_capacity() {
+        Locker firstLocker = new Locker(10);
+        PrimaryLockerRobot robot = new PrimaryLockerRobot(asList(firstLocker, new Locker(15)));
+
+        Bag myBag = new Bag();
+        Ticket ticket = robot.save(myBag);
+
+        assertNotNull(ticket);
+        assertSame(myBag, firstLocker.pickUpBy(ticket));
+    }
+}
