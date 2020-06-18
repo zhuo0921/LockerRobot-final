6318176c88d061c0d256de392d4e59244d3888e2
Yafei Chen <yafchen@thoughtworks.com>
2020-06-18 08:50:11 +0800

    [yafei]throw locker full exception when both lockers are full

--- src/test/java/com/tw/SmartLockerRobotTest.java
+++ src/test/java/com/tw/SmartLockerRobotTest.java
@@ -1,6 +1,5 @@
 package com.tw;
 
-//        3. Given robot管理两个locker，剩余容量都为2; When robot存包，Then 成功存入第一个locker，返回票据
 //        4. Given robot管理两个locker，且两个locker都存满了; When robot存包，Then 存包失败，提示locker已满
 //        5. Given robot管理两个locker，且拿到有效票; When robot取包，Then 取包成功
 //        6. Given robot管理两个locker，且拿到伪造票; When robot取包，Then 取包失败，提示非法票据
@@ -46,5 +45,12 @@ public class SmartLockerRobotTest {
 
         assertNotNull(ticket);
         assertSame(myBag, firstLocker.pickUpBy(ticket));
+    }
+
+    @Test(expected = LockerIsFullException.class)
+    public void should_throw_LockerIsFullException_when_save_bag_given_robot_manage_two_lockers_1st_capacity_is_0_and_2nd_is_0() {
+        SmartLockerRobot robot = new SmartLockerRobot(asList(new Locker(0), new Locker(0)));
+
+        robot.save(new Bag());
     }
 }
